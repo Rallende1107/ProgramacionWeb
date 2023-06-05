48a8be757acf259f7ba13132be4875df1b1b671e
Rene Allende <rene.allende15@gmail.com>
2023-06-05 18:37:01 -0400

    ignore modificado

--- applications/carrito_dos_punto_cero/models.py
+++ applications/carrito_dos_punto_cero/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# class Image(models.Model):
-#     # anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
-#     image_url = models.URLField()
-#     small_image_url = models.URLField()
-#     large_image_url = models.URLField()
+class Image(models.Model):
+    # anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    image_url = models.URLField()
+    small_image_url = models.URLField()
+    large_image_url = models.URLField()
 
-#     def __str__(self):
-#         return self.image_url
+    def __str__(self):
+        return self.image_url
